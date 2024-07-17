763d642e41810b9cc135258bab732fa80bbfbc0a
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2024-07-17 15:22:02 +0200

    added the remove items and clear all the items

--- session/index.php
+++ session/index.php
@@ -6,13 +6,30 @@ session_start();
 if(isset($_POST['submit'])) {
     // Validate and sanitize inputs
     $name = htmlspecialchars($_POST['name']);
-    $url = htmlspecialchars($_POST['url']);
+    $url = filter_var($_POST['url'], FILTER_SANITIZE_URL);
+
+    // Validate URL
+    if (filter_var($url, FILTER_VALIDATE_URL) === false) {
+        // Invalid URL, handle the error (not shown in this snippet)
+    }
 
     // Initialize or update bookmarks session variable
     if(isset($_SESSION['bookmarks'])) {
         $_SESSION['bookmarks'][$name] = $url;
     } else {
         $_SESSION['bookmarks'] = array($name => $url);
+    }
+}
+
+// Handle actions (clearing or deleting bookmarks)
+if(isset($_GET['action'])) {
+    if ($_GET['action'] === 'clear') {
+        unset($_SESSION['bookmarks']);
+    } elseif ($_GET['action'] === 'delete' && isset($_GET['name'])) {
+        $nameToDelete = $_GET['name'];
+        if (isset($_SESSION['bookmarks'][$nameToDelete])) {
+            unset($_SESSION['bookmarks'][$nameToDelete]);
+        }
     }
 }
 ?>
@@ -25,45 +42,45 @@ if(isset($_POST['submit'])) {
     <title>Bookmarker</title>
     <link rel="stylesheet" href="https://bootswatch.com/5/cyborg/bootstrap.min.css">
     <style>
-    /* Basic styling for the navbar */
-    body {
-        font-family: Arial, sans-serif;
-        margin: 0;
-    }
-    nav {
-        background-color: #333;
-        overflow: hidden;
-    }
-    nav ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-    }
-    nav ul li {
-        float: left;
-    }
-    nav ul li a {
-        display: block;
-        color: white;
-        text-align: center;
-        padding: 14px 20px;
-        text-decoration: none;
-    }
-    nav ul li a:hover {
-        background-color: #555;
-    }
-    /* Right-aligned content */
-    .navbar-right {
-        float: right;
-    }
-    .navbar-right a {
-        display: block;
-        color: white;
-        text-align: center;
-        padding: 14px 20px;
-        text-decoration: none;
-    }
+        /* Basic styling for the navbar */
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+        }
+        nav {
+            background-color: #333;
+            overflow: hidden;
+        }
+        nav ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+        nav ul li {
+            float: left;
+        }
+        nav ul li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 20px;
+            text-decoration: none;
+        }
+        nav ul li a:hover {
+            background-color: #555;
+        }
+        /* Right-aligned content */
+        .navbar-right {
+            float: right;
+        }
+        .navbar-right a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 20px;
+            text-decoration: none;
+        }
     </style>
 </head>
 <body>
@@ -72,18 +89,14 @@ if(isset($_POST['submit'])) {
     <ul>
         <li><a href="#">Bookmarker</a></li>
         <li><a href="index.php">Home</a></li>
-
-        <!-- Right-aligned content -->
-        <div class="navbar-right">
-            <a href="index.php?action=clear">Clear All</a>
-        </div>
+        <li class="navbar-right"><a href="index.php?action=clear">Clear All</a></li>
     </ul>
 </nav>
 
 <div class="container">
     <div class="row">
         <div class="col-md-7">
-            <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
                 <div class="form-group">
                     <label for="name">Website Name</label>
                     <input type="text" class="form-control" name="name" id="name" required>
@@ -99,7 +112,10 @@ if(isset($_POST['submit'])) {
             <?php if (isset($_SESSION['bookmarks'])) : ?>
                 <ul class="list-group">
                     <?php foreach($_SESSION['bookmarks'] as $name => $url) : ?>
-                        <li class="list-group-item"><a href="<?php echo $url; ?>" target="_blank"><?php echo $name; ?></a></li>
+                        <li class="list-group-item">
+                            <a href="<?php echo htmlspecialchars($url); ?>" target="_blank"><?php echo htmlspecialchars($name); ?></a>
+                            <a href="index.php?action=delete&name=<?php echo urlencode($name); ?>">[X]</a>
+                        </li>
                     <?php endforeach; ?>
                 </ul>
             <?php endif; ?>
